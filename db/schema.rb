b917fe7194f17d3706217b9b6a78a5db97184e08
OllyGilbert <olly.gilbert@gmail.com>
2014-10-21 13:24:10 +0100

    event_calendar generated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141021112211) do
+ActiveRecord::Schema.define(:version => 20141021121711) do
 
   create_table "classrooms", :force => true do |t|
     t.string   "name"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(:version => 20141021112211) do
   create_table "courses_users", :id => false, :force => true do |t|
     t.integer "user_id"
     t.integer "course_id"
+  end
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.datetime "start_at"
+    t.datetime "end_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "locations", :force => true do |t|
